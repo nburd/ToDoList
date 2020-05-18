3089d79e137d03c4c7ac6e85b3980f3a8cfcb63d
Natalia Burdukova <nat.burdukova@gmail.com>
2020-05-18 17:23:32 +0300

    [TaskListController] class component ->  pure function component

--- src/TaskListController.js
+++ src/TaskListController.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import AddTaskForm from "./AddTaskForm";
 import TaskList from "./TaskList";
 import TasksContext from "./contexts/TasksContext";
@@ -6,52 +6,40 @@ import TasksLogic from "./TasksLogic";
 import LocalizationContext from "./contexts/LocalizationContext";
 import strings from "./strings";
 
-class TaskListController extends React.Component {
-  static contextType = LocalizationContext;
-  constructor(props) {
-    super(props);
-    console.log('AAA!',  )
-    this.state = {
-      tasks: JSON.parse(window.localStorage.getItem('tasks')) || [],
-    };
-  }
+const TaskListController = () => {
+  const [tasks, setTasks] = useState(
+    JSON.parse(window.localStorage.getItem("tasks")) || []
+  );
+  const [validation, setValidation] = useState();
 
-  handleAdd = (task) => {
-    if (task.name && task.date) {
-      this.setState({
-        tasks: TasksLogic.addToTop(this.state.tasks, task),
-        validation: null,
-      });
-    } else {
-      this.setState({
-        validation: strings[this.context].validationError
-      });
-    }
-  };
+  const locale = useContext(LocalizationContext);
+  const localeStings = strings[locale];
 
-  handleChange = (task) => {
-    console.log('handleChange', task);
-    this.setState({ tasks: TasksLogic.changeTask(this.state.tasks, task) });
-  };
-
-  render() {
-    window.localStorage.setItem('tasks', JSON.stringify(this.state.tasks));
-    return (
-      <TasksContext.Provider
-        value={{
-          tasks: this.state.tasks,
-          validation: this.state.validation,
-          handleAdd: this.handleAdd,
-          handleChange: this.handleChange,
-        }}
-      >
-        <div>
-          <AddTaskForm />
-          <TaskList />
-        </div>
-      </TasksContext.Provider>
-    );
-  }
-}
+  window.localStorage.setItem("tasks", JSON.stringify(tasks));
+  return (
+    <TasksContext.Provider
+      value={{
+        tasks: tasks,
+        validation: validation,
+        handleAdd: (task) => {
+          if (task.name && task.date) {
+            setTasks(TasksLogic.addToTop(tasks, task));
+            setValidation(null);
+          } else {
+            setValidation(localeStings.validationError);
+          }
+        },
+        handleChange: (task) => {
+          console.log("handleChange", task);
+          setTasks(TasksLogic.changeTask(tasks, task));
+        },
+      }}>
+      <div>
+        <AddTaskForm />
+        <TaskList />
+      </div>
+    </TasksContext.Provider>
+  );
+};
 
 export default TaskListController;
